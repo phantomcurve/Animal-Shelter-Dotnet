b5ce734faf63dfe87a209b892cdcb9a46c0eb009
Tim Roth <timdroth@gmail.com>
2021-09-29 21:06:33 -0700

    create functional project framework
    Co-Authored-By: Amerik Velasquez<amerik.velasquez22@gmail.com>

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public int DateOfAdmittance { get; set; }
+    public string Breed { get; set; }
+  }
+}
